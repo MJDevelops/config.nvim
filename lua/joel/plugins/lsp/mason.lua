aa71129ef3ca358d295be4c788e1efcb7da1537b
MJDevelops <joey.meierkord@gmail.com>
2023-09-26 21:17:45 +0200

    Add svelte lsp and configuration

--- lua/joel/plugins/lsp/mason.lua
+++ lua/joel/plugins/lsp/mason.lua
@@ -35,6 +35,7 @@ return {
 				"lua_ls",
 				"emmet_ls",
 				"pyright",
+				"svelte",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
