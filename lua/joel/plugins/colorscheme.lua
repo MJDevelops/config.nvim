4001d2762a1bfa6a3c3e4bf67f2789f0ff007337
MJDevelops <joey.meierkord@gmail.com>
2023-12-03 03:46:32 +0100

    Various changes

--- lua/joel/plugins/colorscheme.lua
+++ lua/joel/plugins/colorscheme.lua
@@ -3,9 +3,10 @@ return {
 		"morhetz/gruvbox",
 		lazy = false,
 		priority = 1000,
-		opts = {},
 		config = function()
-			vim.cmd([[colorscheme gruvbox]])
+			vim.cmd("let g:gruvbox_transparent_bg = 1")
+			vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+			vim.cmd("colorscheme gruvbox")
 		end,
 	},
 }
--- lua/joel/plugins/nvim-treesitter.lua
+++ lua/joel/plugins/nvim-treesitter.lua
@@ -24,6 +24,7 @@ return {
 					"json",
 					"javascript",
 					"typescript",
+					"rust",
 					"tsx",
 					"yaml",
 					"html",
