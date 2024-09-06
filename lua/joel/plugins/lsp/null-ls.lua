2ca3c743c25f6a7698191c0a5f93d346785d034b
MJDevelops <joey.meierkord@gmail.com>
2024-09-07 00:51:02 +0200

    Configure cpp formatter

--- lua/joel/plugins/lsp/null-ls.lua
+++ lua/joel/plugins/lsp/null-ls.lua
@@ -26,6 +26,7 @@ return {
 					extra_filetypes = { "svelte" },
 				}), -- js/ts formatter
 				formatting.stylua, -- lua formatter
+				formatting.clang_format, -- cpp formatter
 				diagnostics.eslint_d.with({ -- js/ts linter
 					condition = function(utils)
 						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
